c60c17dcc5fd4f9bbf64f14ed51f5968f6b834d6
Seweryn Zielas <seweryn.zielas@mobica.com>
2023-01-15 17:43:19 +0100

    works for first and second format of json

--- check_for_extension.py
+++ check_for_extension.py
@@ -13,40 +13,66 @@ gpus = {}
  
 # iterate over files in
 # that directory
-for filename in os.listdir(directory):
-    gpus[filename.replace(".json", "")] = ''
-    full_filename = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(full_filename):
-        json_files.append(full_filename)
-        
+def main():
+    search_for_json_files(directory, json_files)
+            
+    for json_filename in json_files:
+        search_json_for_extensions(json_filename, gpus)
 
-for json_filename in json_files:
+
+    print(gpus)
+        
+    write_to_csv("test.csv", gpus)
+
+def search_for_json_files(directory, json_files):
+    for filename in os.listdir(directory):
+        gpus[filename.replace(".json", "")] = ''
+        full_filename = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(full_filename):
+            json_files.append(full_filename)
+
+def search_json_for_extensions(json_filename, gpus):
     f = open(json_filename)
     value = ""
     data = json.load(f)
-    for first_layer in data:
-        if first_layer == "extensions":
-            for extension in data[first_layer]:
-                for key in extension:
-                    if key == "extensionName":
-                        if extension[key] in searched_extensions:
-                            value = value + ' ' + extension[key]
+    if "capabilities" not in data:
+        for first_layer in data:
+            if first_layer == "extensions":
+                for extension in data[first_layer]:
+                    for key in extension:
+                        if key == "extensionName":
+                            if extension[key] in searched_extensions:
+                                value = value + ' ' + extension[key]
+    else:
+        for first_layer in data:
+            if first_layer == "capabilities":
+                for second_layer in data[first_layer]:
+                    if second_layer == "device":
+                        for third_layer in data[first_layer][second_layer]:
+                            if third_layer == "extensions":
+                                for extension in data[first_layer][second_layer][third_layer]:
+                                    if extension in searched_extensions:
+                                        value = value + ' ' + extension
     for gpu in gpus:
         if gpu in json_filename:
             gpus[gpu] = value
+    
                                 
+            
 
-print(gpus)
+def write_to_csv(filename, gpus):
+    gpus_info = ["GPU", "Extensions"]
+
+    with open(filename, 'w', encoding='UTF8', newline='') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+        writer.writerow(gpus_info)
+
+        for key, value in gpus.items():
+            row = [key, value]
+            writer.writerow(row)
     
-gpus_info = ["GPU", "Extensions"]
 
-with open('test.csv', 'w', encoding='UTF8', newline='') as f:
-    # create the csv writer
-    writer = csv.writer(f)
-    writer.writerow(gpus_info)
-    
-    for key, value in gpus.items():
-        row = [key, value]
-        writer.writerow(row)
-
+if __name__ == "__main__":
+    main()
